a9322d7a0070b70430795effa829979fd0a12ff1
Sowmiyadevi05 <abhi05.be@gmail.com>
2024-12-21 09:40:59 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,88 @@
+const Bar = document.getElementById('bar');
+const Navbar = document.getElementById('navbar');
+
+if (bar) {
+    Bar.addEventListener('DOMContentLoaded', () => {
+        Navbar.classList.add("active");
+    })
+    
+}
+
+function imageZoom(imgID, resultID) {
+  var img, lens, result, cx, cy;
+
+  // Get references to elements
+  img = document.getElementById(imgID);
+  result = document.getElementById(resultID);
+
+  /* Create lens: */
+  lens = document.createElement("DIV");
+  lens.setAttribute("class", "img-zoom-lens");
+
+  /* Insert lens: */
+  img.parentElement.insertBefore(lens, img);
+
+  /* Calculate the ratio between result DIV and lens: */
+  cx = result.offsetWidth / lens.offsetWidth;
+  cy = result.offsetHeight / lens.offsetHeight;
+
+  /* Set background properties for the result DIV */
+  result.style.backgroundImage = "url('" + img.src + "')";
+  result.style.backgroundSize = (img.width * cx) + "px " + (img.height * cy) + "px";
+
+  /* Execute a function when someone moves the cursor over the image, or the lens: */
+  lens.addEventListener("mousemove", moveLens);
+  img.addEventListener("mousemove", moveLens);
+
+  // Handle touch screens (optional)
+  if ("ontouchstart" in document.documentElement) {
+    lens.addEventListener("touchmove", moveLens);
+    img.addEventListener("touchmove", moveLens);
+  }
+
+  function moveLens(e) {
+    var pos, x, y;
+
+    /* Prevent any other actions that may occur when moving over the image */
+    e.preventDefault();
+
+    /* Get the cursor's x and y positions: */
+    pos = getCursorPos(e);
+
+    /* Calculate the position of the lens: */
+    x = pos.x - (lens.offsetWidth / 2);
+    y = pos.y - (lens.offsetHeight / 2);
+
+    /* Prevent the lens from being positioned outside the image: */
+    x = Math.max(0, Math.min(x, img.width - lens.offsetWidth)); // Constrain to image boundaries
+    y = Math.max(0, Math.min(y, img.height - lens.offsetHeight));
+
+    /* Set the position of the lens: */
+    lens.style.left = x + "px";
+    lens.style.top = y + "px";
+
+    /* Display what the lens "sees": */
+    result.style.backgroundPosition = "-" + (x * cx) + "px -" + (y * cy) + "px";
+  }
+
+  function getCursorPos(e) {
+    var a, x = 0, y = 0;
+    e = e || window.event;
+
+    /* Get the x and y positions of the image: */
+    a = img.getBoundingClientRect();
+
+    /* Calculate the cursor's x and y coordinates, relative to the image: */
+    x = e.clientX - a.left;
+    y = e.clientY - a.top;
+
+    /* Consider any page scrolling: */
+    x -= window.pageXOffset;
+    y -= window.pageYOffset;
+
+    return { x: x, y: y };
+  }
+}
+
+// Call the imageZoom function to activate zoom on your image
+imageZoom("MainImg", "myresult");
